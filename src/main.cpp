807176f61a1507a83cd47ed91b441471c24b463b
Nicolas Morales Escobar <nico96101@hotmail.com>
2025-08-19 23:34:10 -0700

    Lesson #2: Joining and Detaching Threads

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,12 @@
  * - I'm following the course which is made with C but following the steps in C++.
  * - Threads are pretty easy to "spawn" with C++'s standard library.
  * - I like that we can pass arguments to the thread function implementation
+ * 
+ * Lesson #2: Joining and Detaching Threads
+ * - std::thread::join -> Calling thread will wait till termination of called thread.
+ * - std::thread::detach -> Separates execution of the thread, allowing to continue execution until the thread gets terminated.
+ * - std::terminate (called from respective thread) -> Terminates the calling thread
+ * - Join is a blocking operation, so I'm curious how to make this async work not be blocking
  */
 
 void ThreadExecution(std::string msg);
@@ -17,9 +23,15 @@ int main(int argc, char *argv[])
 {
     static constexpr int8_t threadCount = 10;
     std::thread threads[threadCount];
+
     for (size_t idx = 0; idx < threadCount; idx++)
     {
         threads[idx] = std::thread(ThreadExecution, "Hello World!");
+    }
+
+    for (size_t idx = 0; idx < threadCount; idx++)
+    {
+        threads[idx].join();
     }
 };
 
